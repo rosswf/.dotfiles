d16c3b939feedfc2d3d24161de03a9e316952ad2
rosswf <ross@rossw.co.uk>
2023-01-13 20:09:51 +0000

    Add keymaps for moving lines and yanking

--- nvim/.config/nvim/lua/ross/remap.lua
+++ nvim/.config/nvim/lua/ross/remap.lua
@@ -1,4 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- moves lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "Q", "<nop>")
