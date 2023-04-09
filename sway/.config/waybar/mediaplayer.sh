b416f38c1b1802b32fbeaf57956bbe3b6cc05326
rosswf <ross@rossw.co.uk>
2023-04-09 17:30:36 +0100

    Fix icon spacing

--- sway/.config/waybar/mediaplayer.sh
+++ sway/.config/waybar/mediaplayer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 player_status=$(playerctl -p ncspot status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
-    echo "ﱘ $(playerctl -p ncspot metadata artist) - $(playerctl -p ncspot metadata title)"
+    echo " ﱘ $(playerctl -p ncspot metadata artist) - $(playerctl -p ncspot metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo "ﱙ $(playerctl -p ncspot metadata artist) - $(playerctl -p ncspot metadata title)"
+    echo " ﱙ $(playerctl -p ncspot metadata artist) - $(playerctl -p ncspot metadata title)"
 fi
