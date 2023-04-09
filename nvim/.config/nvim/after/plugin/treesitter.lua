33e87ae4aabf0f877e8871dcdbe2b9798eb7a95c
Ross <ross@rossw.co.uk>
2023-04-09 14:05:40 +0100

    Rename help to vimdoc

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "markdown", "html", "css", "go", "lua", "c", "rust", "python" },
+  ensure_installed = { "vimdoc", "javascript", "markdown", "html", "css", "go", "lua", "c", "rust", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
