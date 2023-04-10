bf661e41e3b1b6b6d7bc84f794a26bad02c712fa
rosswf <ross@rossw.co.uk>
2023-04-10 15:53:47 +0100

    Add nvim-treesitter-context

--- nvim/.config/nvim/lua/ross/packer.lua
+++ nvim/.config/nvim/lua/ross/packer.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   use 'folke/tokyonight.nvim'
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use ('nvim-treesitter/playground')
+  use 'nvim-treesitter/nvim-treesitter-context'
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
